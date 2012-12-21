aba00b0f2b5da685775e05b950ce75b16bcf49cd
Matt Adelman <madelman@wesleyan.edu>
2012-12-21 16:29:00 -0500

    Second commit

--- myaddons.js
+++ myaddons.js
@@ -23,41 +23,12 @@ $(document).ready(function() {
         $('#maths').removeClass('active');
         $('#photos').removeClass('active');
     });
-    $('#africa').mouseenter(function() {
-        $('#africa').fadeTo('fast',0.7);
+    // For photos
+    $('.hover').mouseenter(function() {
+        $(this).fadeTo('fast',0.7);
     });
-    $('#africa').mouseleave(function() {
-        $('#africa').fadeTo('fast',0);
-    });
-    $('#belize').mouseenter(function() {
-        $('#belize').fadeTo('fast',0.7);
-    });
-    $('#belize').mouseleave(function() {
-        $('#belize').fadeTo('fast',0);
-    });
-    $('#aruba').mouseenter(function() {
-        $('#aruba').fadeTo('fast',0.7);
-    });
-    $('#aruba').mouseleave(function() {
-        $('#aruba').fadeTo('fast',0);
-    });
-    $('#home').mouseenter(function() {
-        $('#home').fadeTo('fast',0.7);
-    });
-    $('#home').mouseleave(function() {
-        $('#home').fadeTo('fast',0);
-    });
-    $('#bvi').mouseenter(function() {
-        $('#bvi').fadeTo('fast',0.7);
-    });
-    $('#bvi').mouseleave(function() {
-        $('#bvi').fadeTo('fast',0);
-    });
-    $('#galapagos').mouseenter(function() {
-        $('#galapagos').fadeTo('fast',0.7);
-    });
-    $('#galapagos').mouseleave(function() {
-        $('#galapagos').fadeTo('fast',0);
+    $('.hover').mouseleave(function() {
+        $(this).fadeTo('fast',0);
     });
 
 
